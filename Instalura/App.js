baf691d8b9ed515bf0664ce5d15e8f50b91c6dcf
Junior <junior.jaava@gmail.com>
2020-06-20 23:19:48 +0200

    Working with stylesheet

--- Instalura/App.js
+++ Instalura/App.js
@@ -3,22 +3,32 @@ import React, {Fragment} from 'react';
 import {
   Text, 
   Image, 
-  ScrollView} from 'react-native';
+  ScrollView,
+  Dimensions,
+  StyleSheet} from 'react-native';
 
+const largura = Dimensions.get("screen").width;
 const App = () => {
   return (
     <ScrollView>
       <Text>Junior</Text>
       <Image 
         source={require("./res/img/alura.jpg")}
-        style={{
-          width:300,
-          height:300 
-        }}
+        style={estilo.imagem}
       />
       <Text>Dani</Text>
-      <Image source={require("./res/img/alura.jpg")}/>
+      <Image 
+        source={require("./res/img/alura.jpg")}
+        style={estilo.imagem}
+      />
     </ScrollView>
   )
 };
+
+const estilo = StyleSheet.create({
+  imagem: {
+    width:largura,
+    height:largura 
+  }
+});
 export default App;
