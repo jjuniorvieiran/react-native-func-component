3989abbb8d792268f7753b521921ad9df5d70f38
Junior <junior.jaava@gmail.com>
2020-06-20 23:05:29 +0200

    Working with Scroll, Text, Image

--- Instalura/App.js
+++ Instalura/App.js
@@ -1,13 +1,24 @@
 
 import React, {Fragment} from 'react';
-import {Text, Image} from 'react-native';
+import {
+  Text, 
+  Image, 
+  ScrollView} from 'react-native';
 
 const App = () => {
   return (
-    <Fragment>
+    <ScrollView>
       <Text>Junior</Text>
+      <Image 
+        source={require("./res/img/alura.jpg")}
+        style={{
+          width:300,
+          height:300 
+        }}
+      />
+      <Text>Dani</Text>
       <Image source={require("./res/img/alura.jpg")}/>
-    </Fragment>
+    </ScrollView>
   )
 };
 export default App;
